e91d78a50d079220bd7d1cceeeb44b6e5834d207
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-04 17:17:42 -0500

    adding .c file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,9 +12,9 @@ int main(void)
 
 	int number;
 
-	for (number = 0; letter < 10; number++)
+	for (number = 0; number < 10; number++)
 	{
-		printf(number"%d", number)
+		printf("%d", number)
 	}
 	printf("\n");
 	return (0);
