187ff9622a8dcf6cab17791d756b4232ecbf7458
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-04 17:07:21 -0500

    adding .c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in order
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+		putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
