0386732db560d8bae1cca7f4d771bfd2003e56d2
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-04 17:15:19 -0500

    adding .c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in order
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int number;
+
+	for (number = 0; letter < 10; number++)
+	{
+		printf(number"%d", number)
+	}
+	printf("\n");
+	return (0);
+}
