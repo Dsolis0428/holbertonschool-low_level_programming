90b6aec4b099d19cbc800e699261fb46606569be
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-04 20:51:50 -0500

    adding .c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-
 	int digi1, digi2;
 
 	for (digi1 = 0; digi1 <= 9; digi1++)
 	{
-		for (digi2 = 0 ; digi2 <= 10; digi2++)
+		for (digi2 = digi1 + 1; digi2 <= 10; digi2++)
 		{
 			putchar((digi1 % 10) + '0');
 			putchar((digi2 % 10) + '0');
@@ -20,8 +19,7 @@ int main(void)
 				continue;
 					putchar(',');
 					putchar(' ');
-				}
-			}
+				
 		}
 	}
 	putchar('\n');
