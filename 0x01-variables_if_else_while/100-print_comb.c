97c44a17054941f29597fc2c303d021702ae8294
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-04 19:52:15 -0500

    adding .c file

---
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints number 0-9 with putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int digi1, digi2;
+
+	for (digi1 = 0; digi1 < 9; digi1++)
+	{
+		for (digi2 = digi1 + 1; digi2 < 10; digi2++)
+		digi1= digi1 % 10;
+		digi2= digi2 % 10;
+		putchar(digi1 + '0');
+		putchar(digi2 + '0');
+
+		if (digi1 == 8 && digi2 ==9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
