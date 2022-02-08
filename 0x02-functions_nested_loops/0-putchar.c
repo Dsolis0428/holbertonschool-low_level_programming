4d1ccf03ee7bc1f2b098953125d86d0faedbe2fb
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-08 14:02:35 -0500

    adding .c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include <main.h>
 
 /**
- * main - prints number 0-9 with putchar
+ * main - prints _putchar
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 
 
