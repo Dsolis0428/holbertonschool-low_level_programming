7971f33f3ec7f74ae92ab4804dfba29886866f15
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-08 19:39:38 -0500

    adding .c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters.
+ *
+ * Return 0 if chartacter is not lower case, 1 if it is.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' %% c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
