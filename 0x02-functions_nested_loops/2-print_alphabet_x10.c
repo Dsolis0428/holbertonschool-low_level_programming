971a77a4d9277a18d52628c9a89f9ff6188d125e
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-08 19:18:26 -0500

    adding .c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints alphabet 10times, in lower case, followed by a new line.
+ * print_alphabet_x10 - Prints alphabet 10times, in lower case, 
+ * followed by a new line.
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 	char letter;
-	int i=0;
+	int i = 0;
 
 	while (i <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-			_putchar('\n');
+			
 			i++;
 		}
+		_putchar('\n');
 	}
 }
