b0faa43b25dd1d74fc29cea7d799163469bd0f61
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-10 16:36:11 -0600

    adding .c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/**
- * print_most_numbers - Prints the numbers from 0-9 but not 2 and 4.
- */
 
-void print_most_numbers(void);
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
+void print_most_numbers(void)
 {
 	int num;
 
