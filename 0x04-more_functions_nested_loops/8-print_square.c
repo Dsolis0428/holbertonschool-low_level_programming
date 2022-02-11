7ebcf8fe54dad4bfb8abfd4b30f156164756a863
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-10 19:35:50 -0600

    adding .c file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of desire input
+ * @size: size of square
+ *
+ * Return: void
+ */
+
+
+void print_diagonal(int n)
+{
+	int line, square;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (line = 0; line < size; line++)
+	{
+		for (square = 0; square < (size); b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
