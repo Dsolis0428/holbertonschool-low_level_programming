a7f3f7d42924f26722545588750f33b3deb2bec4
root <root@LAPTOP-OUPP41VT.localdomain>
2022-02-17 17:20:46 -0600

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,21 +5,26 @@ include "main.h"
  *
  * @dest: destination string.
  * @src: source string.
+ * 
+ * Return: pointer of the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i=0;
 	int j;
+
 	while (dest[i])
 	{
 		i++;
 	}
+
 	for (j = 0; src[j]; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
+	
 	dest[dlen] = '\0';
 	return (dest);
 }
