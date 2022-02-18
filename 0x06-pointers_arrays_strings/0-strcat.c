7ccbb1def63d0004def7ec5f4677bc7e2552afe1
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-18 00:29:19 -0600

    adding .c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,27 @@
 #include "main.h"
-#include<stdio.h>
+
 /**
- * _strcat - return size of strlen
- * @dest: pointer to array 1
- * @src: pointer to array 2
- * Return: dest
+ *_strcat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *Return: string.
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
-	int c = 0;
+	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 
 	while (src[j] != '\0')
 	{
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
 
-	for (c = 0; c < (j + i); c++)
-	{
-		dest[i + c] = src[c];
-	}
+	dest[i] = '\0';
 
-	dest[j + i] = '\0';
 	return (dest);
 }
