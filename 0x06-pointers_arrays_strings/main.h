b582f388571ba68d32f81b05a0d24fb82f5f67d2
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-18 00:23:44 -0600

    first commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,13 +11,13 @@
 #include <stdlib.h>
 
 char *_strcat(char *dest, char *src);
+int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *string_toupper(char *n);
 void reverse_array(int *a, int n);
-char *cap_string(char *);
+char *string_toupper(char *);
+char *cap_string(char *s);
 char *leet(char *);
-char *_strcpy(char *dest, char *src);
 
 #endif
