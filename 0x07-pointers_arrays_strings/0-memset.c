cf3be88809b4512da0d07230da1ed3473d2c4635
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-21 14:54:47 -0600

    adding .c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- *_memset - insert n times the content of b into s.
- *@s: pointer to memory of are being files.
- *@b: the string to insert.
- *@n: number of time to insert.
- *Return: pointer s.
+ * _memset - fills memory with a constant byte
+ * @s: memory space to be filled
+ * @b: byte to fill with
+ * @n: number of spaces to fill
+ *
+ * Return: pointer to memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		*(s + i) = b;
 	}
-
 	return (s);
 }
