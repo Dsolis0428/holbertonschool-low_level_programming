0f4c9ee2a297bffea150dbd6411bd975ab73ba81
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-02-21 14:51:04 -0600

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _memset- fills memory with a constant byte.
- * @s: pointer to area of memory being filled.
- * @b: character to fill memory. 
- * @n: number of times to fill.  
- * 
+ *_memset - insert n times the content of b into s.
+ *@s: pointer to memory of are being files.
+ *@b: the string to insert.
+ *@n: number of time to insert.
+ *Return: pointer s.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
