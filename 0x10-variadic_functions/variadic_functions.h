06ae9a049603baa2522781e5b456380d01e488ae
Dsolis0428 <98237217+Dsolis0428@users.noreply.github.com>
2022-03-10 22:06:50 -0600

    first commit

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,22 @@
+#ifndef VARIADIC_FUCNTIONS
+#define VARIADIC_FUNCTIONS
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct format - Types.
+ * @form: Type symboles.
+ * @f: Function pointer to the corresponding type function.
+ **/
+typedef struct format
+{
+char form;
+void (*f)(va_list);
+} format_t;
+void print_all(const char * const format, ...);
+
+#endif
