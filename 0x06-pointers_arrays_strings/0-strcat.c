eb9dbe416743c350ef7d43468fa7b1e5ae58b462
Dylan <dylansolis0428@gmail.com>
2022-09-23 21:51:39 +0000

    betty style fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,28 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Dylan ALberto Solis Martin (Bimbsy)
+ */
+
 #include "main.h"
 
 /**
- *_strcat - concatinate 2 strings.
- *@dest: first string.
- *@src: second string.
- *Return: string.
+ * _strcat - function that concatenates two strings.
+ * @dest: pointer to address of string, where second string is being added.
+ * @src: pointer to address of string being added to first string.
+ * Return: Pointer dest.
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int index = 0, end_of_dest = 0;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
+	while (dest[index])
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		index[end_of_dest] = dest[index];
+		index++;
 	}
-
-	dest[i] = '\0';
-
+	for (index = 0; src[index]; index++)
+	{
+		dest[end_of_dest++] = src[index];
+	}
 	return (dest);
 }
