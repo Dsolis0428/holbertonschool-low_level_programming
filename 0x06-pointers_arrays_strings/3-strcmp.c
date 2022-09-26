76bc03e8037a82f87772857041b57c13017ae620
Dylan <dylansolis0428@gmail.com>
2022-09-26 17:18:06 +0000

    code update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,20 @@
+/*
+ * File: 3-strcmp.c
+ * Auth: Dylan Alberto Solis Martin (Bimbsy)
+ */
+
 #include "main.h"
 
 /**
  * _strcmp - compare two strings.
- *@s1: string 1.
- *@s2: string 2.
- *Return: int.
+ *@s1: A pointer to the first string to be compared.
+ *@s2: A pointer to the second string to be compare.
+ *
+ * Return: int cmp. If string 1 < string 2, cmp = to negative diffrence
+ *         of the first unmatched characters.
+ *         If string 1 == string 2, cmp is euqal to z.
+ *         If string 1 is > string 2, cmp is equal to the postive diffrence
+ *         of the first unmatched character.
  */
 
 int _strcmp(char *s1, char *s2)
