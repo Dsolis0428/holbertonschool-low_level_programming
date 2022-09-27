cf7b6fab57ca987f97385b834bc3b789799284da
Dylan <dylansolis0428@gmail.com>
2022-09-27 17:07:18 +0000

    code fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  * memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+void *_memset(void *s, int n, unsigned int n)
 {
 	unsigned int index;
 
