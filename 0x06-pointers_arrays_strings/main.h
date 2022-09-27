82a5868937d9b48d3103bfd275a30a60a0b19c15
Dylan <dylansolis0428@gmail.com>
2022-09-27 00:02:11 +0000

    code fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ char *leet(char *n);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);     
+void print_buffer(char *b, int size);
+int _putchar(char c);
 #endif
