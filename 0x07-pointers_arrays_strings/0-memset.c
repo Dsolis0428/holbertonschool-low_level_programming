7001fac20a357a2b485a850ec37f26c0b8214b83
Dylan <dylansolis0428@gmail.com>
2022-09-27 17:05:21 +0000

    code change

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,10 +17,11 @@
 void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
-		memory[index] = value;
+	{
+		s[index] = b;
+	}
 
-	return (memory);
+	return (s);
 }
