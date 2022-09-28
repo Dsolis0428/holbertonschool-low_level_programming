665916bd5f3554a39f1e8ffe06fea7cb54f1e67b
Dylan <dylansolis0428@gmail.com>
2022-09-28 16:45:20 +0000

    code update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,30 @@
+/*
+ * File: 2-strchr.c
+ * Auth: Dylan Alberto Solis Martin (Bimbsy)
+ */
+
 #include "main.h"
 
 /**
- *_strchr - find the first occurence of a char.
- *@c: the char to be found.
- *@s: the string to search in.
- *Return: a pointer to the first occurence of char c in s or NULL.
+ * _strchr - Find the first occurence of a char.
+ * @c: Pointer to the char to be found.
+ * @s: Pointer to the string to search in.
+ * Return: A pointer to the first occurence of char c in s or NULL.
  */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] == c)
 			break;
 		i++;
 	}
 	if (s[i] == c)
+	{
 		return (s + i);
+	}
 	return (0);
 }
