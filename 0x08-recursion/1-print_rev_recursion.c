5a9647c98086c6d6d94fe39e51829d4a68c2834c
Dylan <dylansolis0428@gmail.com>
2022-09-29 23:28:56 +0000

    code update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,10 @@
+/*
+ * File: 1-print_rev_recursion
+ * Auth: Dylan Alberto Solis Martin (Bimbsy)
+ */
+
 #include "main.h"
+
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
